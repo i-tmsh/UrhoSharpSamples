b247e70af7e722b08ba8252fc2a454e6fd8237f5
world_wide_anito <tomohiro.shota@gmail.com>
2018-05-13 21:56:47 +0900

    Impl HelloUrho

---
+++ HelloUrho/HelloUrho/HelloUrho/HelloUrhoApp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Urho;
+using Urho.Gui;
+
+namespace HelloUrho
+{
+    class HelloUrhoApp : Urho.Application
+    {
+        public HelloUrhoApp(ApplicationOptions options) : base(options)
+        {
+        }
+
+        protected override void Start()
+        {
+            base.Start();　// 必須
+
+            var helloText = new Text()
+            {
+                Value = "Hello Urho",
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center,
+            };
+
+            // フォントの指定は必須
+            helloText.SetFont(CoreAssets.Fonts.AnonymousPro, 80f);
+
+            this.UI.Root.AddChild(helloText);
+        }
+    }
+}
--- HelloUrho/HelloUrho/HelloUrho/MainPage.xaml.cs
+++ HelloUrho/HelloUrho/HelloUrho/MainPage.xaml.cs
@@ -3,15 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Urho;
+using Urho.Forms;
 using Xamarin.Forms;
 
 namespace HelloUrho
 {
-	public partial class MainPage : ContentPage
-	{
-		public MainPage()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class MainPage : ContentPage
+    {
+        private UrhoSurface urhoSurface;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            urhoSurface = new UrhoSurface();
+
+            // UrhoViewはMainPage.xamlで定義済
+            this.FindByName<ContentView>("UrhoView").Content = urhoSurface;
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            await urhoSurface.Show<HelloUrhoApp>(new ApplicationOptions(assetsFolder: null));
+        }
+    }
 }
